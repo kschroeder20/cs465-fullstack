4684d5532f09544d8f015ad4684afc000b92165d
Kevin Schroeder <schroederkevin94@gmail.com>
2021-12-20 17:52:30 -0500

    Make Travel Page Render Using Handlebars

--- app.js
+++ app.js
@@ -3,14 +3,24 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+var hbs = require('hbs');
 
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
+
+var indexRouter = require('./app_server/routes/index');
+// var usersRouter = require('./app_server/routes/users');
+var aboutRouter = require('./app_server/routes/about');
+var travelRouter = require('./app_server/routes/travel');
+var roomsRouter = require('./app_server/routes/rooms');
+var newsRouter = require('./app_server/routes/news');
 
 var app = express();
 
 // view engine setup
-app.set('views', path.join(__dirname, 'views'));
+app.set('views', path.join(__dirname, 'app_server', 'views'));
+
+// register handlebars partials (https://www.npmjs.com/package/hbs)
+hbs.registerPartials(path.join(__dirname, 'app_server', 'views/partials'));
+    
 app.set('view engine', 'hbs');
 
 app.use(logger('dev'));
@@ -20,7 +30,11 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
-app.use('/users', usersRouter);
+// app.use('/users', usersRouter);
+app.use('/about', aboutRouter);
+app.use('/travel', travelRouter);
+app.use('/rooms', roomsRouter);
+app.use('/news', newsRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
